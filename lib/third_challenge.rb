27a374c85bc434e707e0c0f951dc0c436a4178d5
Ledio Bode <l.bode515@gmail.com>
2018-05-14 01:37:41 +0000

    Done.

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,5 +1,5 @@
 def third_challenge
-  epic_tragedy = {
+ epic_tragedy = {
    :montague => {
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
@@ -14,5 +14,4 @@ def third_challenge
    }
 }
  
-  
 end
